4d9dd6d018b9b12f64841d6b65b43f8b65dd30ae
Anchal Rastogi <anchalrastogi653@gmail.com>
2025-08-03 13:36:07 +0530

    fixed settings.py

--- MyPortfolio/settings.py
+++ MyPortfolio/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATE_DIR=os.path.join(BASE_DIR,'templates')
+TEMPLATE_DIR=BASE_DIR/"templates"
 
 
 # Quick-start development settings - unsuitable for production
@@ -57,7 +57,7 @@ import os
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATE_DIR],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
